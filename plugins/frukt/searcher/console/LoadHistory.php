187a14681de7b22abf52bb38c5183093a301b2dc
fruskate <fruskate@gmail.com>
2021-11-13 14:56:21 +0600

    Чуть изменили импорт в БД

--- plugins/frukt/searcher/console/LoadHistory.php
+++ plugins/frukt/searcher/console/LoadHistory.php
@@ -21,29 +21,50 @@ class LoadHistory extends Command
     public function handle()
     {
         $this->output->title('Импорт начался');
+
+        $c =0;
+        $fp = fopen(plugins_path('frukt/searcher/assets/datasets/search_history_tab.csv'),"r");
+        if($fp){
+            while(!feof($fp)){
+                $content = fgets($fp);
+                if($content)    $c++;
+            }
+        }
+        fclose($fp);
+        $bar = $this->output->createProgressBar($c);
+
+
         $row = 1;
-        if (($handle = fopen(plugins_path('frukt/searcher/assets/datasets/search_history.csv'), "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        if (($handle = fopen(plugins_path('frukt/searcher/assets/datasets/search_history_tab.csv'), "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) {
                 //$num = count($data);
                 //trace_log("$num полей в строке $row:");
                 if ($row > 1) {
-                    \Frukt\Searcher\Models\Item::where('uq', $data[0])->create([
-                        'wbuser_id' => $data[0],
-                        'uq' => $data[1],
-                        'cnt' => $data[2],
-                        'locale' => $data[3],
-                        'weekday' => $data[4],
-                        'time' => $data[5]
-                    ]);
+                    try {
+                        \Frukt\Searcher\Models\Item::where('uq', $data[0])->create([
+                            'wbuser_id' => $data[0],
+                            'uq' => $data[1],
+                            'cnt' => $data[2],
+                            'locale' => $data[3],
+                            'weekday' => $data[4],
+                            'time' => $data[5]
+                        ]);
+                    } catch (\Exception $exception) {
+                        trace_log('Ошибка в данных');
+                        trace_log($data);
+                    }
+
                     /* for ($c=0; $c < $num; $c++) {
                         trace_log($data[$c]);
                     } */
                 }
 
                 $row++;
+                $bar->advance();
             }
             fclose($handle);
         }
+        $bar->finish();
         $this->output->success('Импорт завершён. Загружено '.$row.' строк.');
     }
 
--- plugins/frukt/searcher/console/LoadPopularity.php
+++ plugins/frukt/searcher/console/LoadPopularity.php
@@ -21,25 +21,45 @@ class LoadPopularity extends Command
     public function handle()
     {
         $this->output->title('Импорт начался');
+
+        $c =0;
+        $fp = fopen(plugins_path('frukt/searcher/assets/datasets/query_popularity_tab.csv'),"r");
+        if($fp){
+            while(!feof($fp)){
+                $content = fgets($fp);
+                if($content)    $c++;
+            }
+        }
+        fclose($fp);
+        $bar = $this->output->createProgressBar($c);
+
         $row = 1;
-        if (($handle = fopen(plugins_path('frukt/searcher/assets/datasets/query_popularity.csv'), "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        if (($handle = fopen(plugins_path('frukt/searcher/assets/datasets/query_popularity_tab.csv'), "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) {
                 //$num = count($data);
                 //trace_log("$num полей в строке $row:");
                 if ($row > 1) {
-                    \Frukt\Searcher\Models\Popular::where('name', trim($data[0]))->firstOrCreate([
-                        'name' => trim($data[0]),
-                        'popularity' => $data[1]
-                    ]);
+                    try {
+                        \Frukt\Searcher\Models\Popular::where('name', trim($data[0]))->firstOrCreate([
+                            'name' => trim($data[0]),
+                            'popularity' => $data[1]
+                        ]);
+                    } catch (\Exception $exception) {
+                        trace_log('Ошибка в данных');
+                        trace_log($data);
+                    }
+
                     /* for ($c=0; $c < $num; $c++) {
                         trace_log($data[$c]);
                     } */
                 }
 
                 $row++;
+                $bar->advance();
             }
             fclose($handle);
         }
+        $bar->finish();
         $this->output->success('Импорт завершён. Загружено '.$row.' строк.');
     }
 
